4f57069e6997e4aa807df2286027d46b5407a16f
MohamedElhosaraty <mohamedelhosaraty05@gmail.com>
2025-02-11 04:36:16 +0200

    create utils in core

---
+++ lib/core/utils/app_color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColor {
+  static const primaryColor = Color(0xFF5F33E1);
+  static const secondaryColor = Color(0xFFF5F876);
+
+}
---
+++ lib/core/utils/end_point.dart
@@ -0,0 +1,6 @@
+class EndPoint {
+  static const String baseUrl = "https://todo.iraqsapp.com/";
+  static const String login = "auth/login";
+  static const String signup = "auth/register";
+  static const String token = "token";
+}
